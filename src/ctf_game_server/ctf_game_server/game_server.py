1d40c9c852a9d5cbbc86fe636783c362f8228aba
Naman Aggarwal <namanagg@mit.edu>
2025-11-24 18:09:06 -0500

    updated game server init_pose logic

--- src/ctf_game_server/ctf_game_server/game_server.py
+++ src/ctf_game_server/ctf_game_server/game_server.py
@@ -49,6 +49,11 @@ class GameServer(Node):
         self.rover_pose_subscriptions = {}
         self.server_to_rover_publishers = {}
 
+        self.blue_agents = [agent for agent in self.rovers_list if self.rovers_info[agent]['team'] == 'BLUE']
+        self.red_agents = [agent for agent in self.rovers_list if self.rovers_info[agent]['team'] == 'RED']
+        self.num_agents_blue_team = len(self.blue_agents)
+        self.num_agents_red_team = len(self.red_agents)
+
         self.join_game_topic = "/ctf/join"
         self.subscriber_join_game_topic = self.create_subscription(
                 JoinGameMessage,
@@ -115,7 +120,7 @@ class GameServer(Node):
         if self.num_rovers == 4:
             self.start_game_callback()
         return
-    
+
     def start_game_callback(self):
         # Send initial commanded poses to start the game.
         # Wait for response.
@@ -136,6 +141,52 @@ class GameServer(Node):
         return
     
     def compute_initial_poses(self):
+        return self.reset()
+    
+    def reset(self):
+        blue_team_init_xys = []
+        for agent_iter, blue_agent_num in enumerate(range(self.num_agents_blue_team)):
+            if agent_iter == 0:
+                rand_x = self.np_random.integers(0, self.grid_size)
+                rand_y = self.np_random.integers(0, self.blue_init_spawn_y_lim + 1)
+                rand_theta = self._sample_init_heading(agent_team="Blue", x=rand_x, y=rand_y)
+                self.state["Blue_{}".format(blue_agent_num)] = np.array([rand_x, rand_y, rand_theta])
+                blue_team_init_xys.append((rand_x, rand_y))
+                continue
+            # A while loop to break ties in the x-y location to prevent collision during agent initialization.
+            while (rand_x, rand_y) in blue_team_init_xys:
+                rand_x = self.np_random.integers(0, self.grid_size)
+                rand_y = self.np_random.integers(0, self.blue_init_spawn_y_lim + 1)
+            blue_team_init_xys.append((rand_x, rand_y))
+            rand_theta = self._sample_init_heading(agent_team="Blue", x=rand_x, y=rand_y)
+            self.state["Blue_{}".format(blue_agent_num)] = np.array([rand_x, rand_y, rand_theta])
+
+        red_team_init_xys = []
+        for agent_iter, red_agent_num in enumerate(range(self.num_agents_red_team)):
+            if agent_iter == 0:
+                rand_x = self.np_random.integers(0, self.grid_size)
+                rand_y = self.np_random.integers(self.grid_size - self.red_init_spawn_y_lim - 1, self.grid_size)
+                rand_theta = self._sample_init_heading(agent_team="Red", x=rand_x, y=rand_y)
+                self.state["Red_{}".format(red_agent_num)] = np.array([rand_x, rand_y, rand_theta])
+                red_team_init_xys.append((rand_x, rand_y))
+                continue
+            # A while loop to break ties in the x-y location to prevent collision during agent initialization.
+            while (rand_x, rand_y) in red_team_init_xys:
+                rand_x = self.np_random.integers(0, self.grid_size)
+                rand_y = self.np_random.integers(self.grid_size - self.red_init_spawn_y_lim - 1, self.grid_size)
+            red_team_init_xys.append((rand_x, rand_y))
+            rand_theta = self._sample_init_heading(agent_team="Red", x=rand_x, y=rand_y)
+            self.state["Red_{}".format(red_agent_num)] = np.array([rand_x, rand_y, rand_theta])
+
+        #global_state = [self.state[agent] for agent in self.agents]
+        #obs = {agent: global_state for agent in self.agents} # global_state dict. The prisoner guard example on ParallelEnv has this as a tuple, and you have this as a List currently.
+        obs = self._observations()
+        info = {agent: {} for agent in self.agents}
+        #print("self.agents: {}".format(self.agents))
+        return obs, info
+    
+    def discrete_grid_abstraction_to_highbay_coordinates(self, discrete_x, discrete_y):
+        
         return
 
     def vicon_callback(self, msg, name):
