681fab270f157ce6727889b012aceec8091ed378
seraHam <seras@mit.edu>
2025-12-18 12:23:41 -0500

    Update goal conversion to use dynus state type

--- src/ctf_rover/ctf_rover/publish_global_goal.py
+++ src/ctf_rover/ctf_rover/publish_global_goal.py
@@ -2,6 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import PoseStamped
+from dynus_interfaces.msg import State
 import os
 
 # Publish the goal at self.goal_pose in the global (world) frame
@@ -17,13 +18,14 @@ class PublishGlobalGoal(Node):
 
         # create publisher to terminal global goal topic
         self.publisher = self.create_publisher(
-            PoseStamped,
+            State,
             f"/{self.rover_name}/global_term_goal",
             10
         )
 
         # goal pose in global frame
         self.goal_pose = [2.0, 0.0, 0.0]
+        self.goal_vel = [0.0, 0.0, 0.0]
 
         # create timer
         self.timer = self.create_timer(2.0, self.publish_goal)
@@ -34,19 +36,27 @@ class PublishGlobalGoal(Node):
     def publish_goal(self):
 
         # create goal message
-        msg = PoseStamped()
+        msg = State()
         msg.header.stamp = self.get_clock().now().to_msg()
         msg.header.frame_id = "world"
 
-        msg.pose.position.x = self.goal_pose[0]
-        msg.pose.position.y = self.goal_pose[1]
-        msg.pose.position.z = self.goal_pose[2]
+        # set position
+        msg.pos.x = self.goal_pose[0]
+        msg.pos.y = self.goal_pose[1]
+        msg.pos.z = self.goal_pose[2]
 
-        msg.pose.orientation.x = 0.0
-        msg.pose.orientation.y = 0.0
-        msg.pose.orientation.z = 0.0
-        msg.pose.orientation.w = 1.0
+        # set velocity
+        msg.vel.x = self.goal_vel[0]
+        msg.vel.y = self.goal_vel[1]
+        msg.vel.z = self.goal_vel[2]
 
+        # identity
+        msg.quat.x = 0.0
+        msg.quat.y = 0.0
+        msg.quat.z = 0.0
+        msg.quat.w = 1.0
+
+        # publish goal
         self.publisher.publish(msg)
         self.get_logger().info(f"Published global goal: {self.goal_pose}")
 
