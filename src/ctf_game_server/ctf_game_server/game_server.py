966922a93e1e4b1e1f68ed3112a1f7f778edf37d
seraHam <seras@mit.edu>
2026-04-16 15:32:33 -0400

    Add graph publisher to game server

--- src/ctf_game_server/ctf_game_server/game_server.py
+++ src/ctf_game_server/ctf_game_server/game_server.py
@@ -13,6 +13,8 @@ import tf2_ros
 from tf2_ros import TransformException
 from visualization_msgs.msg import Marker
 from nav_msgs.msg import Odometry
+import networkx as nx
+from geometry_msgs.msg import Point
 
 import copy
 import numpy as np
@@ -92,6 +94,11 @@ class GameServer(Node):
 
         self.marker_pub = self.create_publisher(Marker, 'initial_pose_marker', 10)
 
+        # publish graph every 1.0 seconds
+        self.graph_timer_period = 1.0
+        self.graph_pub = self.create_publisher(Marker, 'ctf_graph', 10)
+        self.graph_pub_timer = self.create_timer(self.graph_timer_period, self.graph_pub_cb)
+
         """
         # Service for rover to request game state
         self.srv = self.create_service(
@@ -100,6 +107,146 @@ class GameServer(Node):
             self.handle_get_state
         ) 
         """
+    
+    ################# BEGIN VISUALIZE GRAPH #################
+    # convert networkx to nodes, edges for markers
+    def nx_to_marker_data(self, G, flag):
+        node_pose_dict = nx.get_node_attributes(G, 'pos')
+
+        nodes = []
+        node_index = {}
+
+        # assign indices
+        for idx, node in enumerate(G.nodes()):
+            x, y = node_pose_dict[node]
+            nodes.append((float(x), float(y)))
+            node_index[node] = idx
+
+        edges = []
+        for u, v in G.edges():
+            edges.append((node_index[u], node_index[v]))
+
+        return nodes, edges, node_pose_dict[flag]
+
+    # publish graph
+    def graph_pub_cb(self):
+
+        # TODO much of this can be initialized once as instance variables
+        if hasattr(self.ctf_env, '_bc') and self.ctf_env._bc is not None:
+            bc = self.ctf_env._bc
+            G_map = bc.G
+
+            red_flag_node = bc.red_flag_L
+            red_flag_2_node = bc.red_flag_R
+            blue_flag_node = self.ctf_env.blue_flag_node
+
+            flag_nodes = np.array([red_flag_node, red_flag_2_node, blue_flag_node])
+
+            flag_idx = 1
+
+            graph_nodes, graph_edges, flag = self.nx_to_marker_data(G_map, flag_nodes[flag_idx]) # TODO fix flag logic
+
+            graph_frame_id = "sim" # vicon frame
+
+            # create node markers
+            node_marker = Marker()
+            node_marker.header.frame_id = graph_frame_id
+            node_marker.header.stamp = self.get_clock().now().to_msg()
+
+            node_marker.ns = "ctf_graph_nodes"
+            node_marker.id = 0
+            node_marker.type = Marker.SPHERE_LIST
+            node_marker.action = Marker.ADD
+
+            node_marker.scale.x = 0.1  # point width
+            node_marker.scale.y = 0.1
+            node_marker.scale.z = 0.1
+
+            node_marker.color.r = 1.0
+            node_marker.color.g = 1.0
+            node_marker.color.b = 0.0
+            node_marker.color.a = 1.0
+
+            # iterate through graph and publish nodes
+            for x, y in graph_nodes:
+
+                # point for each node
+                p = Point()
+                p.x = float(x)
+                p.y = float(y)
+                p.z = 0.0
+                node_marker.points.append(p)
+
+            # publish nodes
+            self.graph_pub.publish(node_marker)
+
+            # create edge markers
+            edge_marker = Marker()
+            edge_marker.header.frame_id = graph_frame_id
+            edge_marker.header.stamp = self.get_clock().now().to_msg()
+
+            edge_marker.ns = "ctf_graph_nodes"
+            edge_marker.id = 1
+            edge_marker.type = Marker.LINE_LIST
+            edge_marker.action = Marker.ADD
+
+            edge_marker.scale.x = 0.05
+
+            edge_marker.color.r = 1.0
+            edge_marker.color.g = 1.0
+            edge_marker.color.b = 0.0
+            edge_marker.color.a = 0.1
+
+            for i, j in graph_edges:
+                p1 = Point()
+                p1.x = float(graph_nodes[i][0])
+                p1.y = float(graph_nodes[i][1])
+                p1.z = 0.0
+
+                p2 = Point()
+                p2.x = float(graph_nodes[j][0])
+                p2.y = float(graph_nodes[j][1])
+                p2.z = 0.0
+
+                edge_marker.points.append(p1)
+                edge_marker.points.append(p2)
+
+            # publish edges
+            self.graph_pub.publish(edge_marker)
+
+            # create flag markers
+            flag_marker = Marker()
+            flag_marker.header.frame_id = graph_frame_id
+            flag_marker.header.stamp = self.get_clock().now().to_msg()
+
+            flag_marker.ns = "ctf_flag"
+            flag_marker.id = 2
+            flag_marker.type = Marker.CYLINDER
+            flag_marker.action = Marker.ADD
+
+            flag_marker.scale.x = 0.2
+            flag_marker.scale.y = 0.2
+            height = 2.0
+            flag_marker.scale.z = height # height
+
+            flag_marker.color.r = 1.0
+            flag_marker.color.g = 0.0
+            flag_marker.color.b = 0.0
+            flag_marker.color.a = 1.0
+
+            # flag location
+            flag_marker.pose.position.x = float(flag[0])
+            flag_marker.pose.position.y = float(flag[1])
+            flag_marker.pose.position.z = height/2  
+
+            flag_marker.pose.orientation.x = 0.0
+            flag_marker.pose.orientation.y = 0.0
+            flag_marker.pose.orientation.z = 0.0
+            flag_marker.pose.orientation.w = 1.0
+
+            # publish nodes
+            self.graph_pub.publish(flag_marker)
+    ################# END VISUALIZE GRAPH #################
     
     # initialize tf from world to map for a given rover
     def initialize_tf(self, rover_name):
